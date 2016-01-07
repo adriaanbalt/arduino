d705538a3a98839a4a9db316e81b9388ba4335b5
adriaanbalt <adriaan@balt.us>
2016-01-07 10:36:51 -0500

    updating spaceship with random led color brightness

--- spaceship-interface.ino
+++ spaceship-interface.ino
@@ -1,33 +1,43 @@
-const int redLed = 5
-const int yellowLed = 4
-const int greenLed = 3
+/*
+ Fade
 
-const int switchPin = 2;
+ This example shows how to fade an LED on pin 9
+ using the analogWrite() function.
 
+ This example code is in the public domain.
+ */
+
+const int greenled = 3;           // the pin that the LED is attached to
+const int yellowled = 5;           // the pin that the LED is attached to
+const int redled = 6;           // the pin that the LED is attached to
+const int switchPin = 2;           // the pin that the LED is attached to
+int redBrightness = 0;    // how bright the LED is
+int greenBrightness = 0;    // how bright the LED is
+int yellowBrightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+// the setup routine runs once when you press reset:
 void setup() {
-    pinMode(redLed, OUTPUT); 
-    pinMode(yellowLed, OUTPUT);
-    pinMode(greenLed, OUTPUT);
+  // declare pin 9 to be an output:
+    pinMode(greenled, OUTPUT);
+    pinMode(yellowled, OUTPUT);
+    pinMode(redled, OUTPUT);
     pinMode(switchPin, INPUT);
 }
 
+// the loop routine runs over and over again forever:
 void loop() {
-  
-    int switchState;
-    
-    switchState = digitalRead( switchPin );
+    fadeLED( greenled, getRandomInt(0, 255) );
+    fadeLED( redled, getRandomInt(0, 255) );
+    fadeLED( yellowled, getRandomInt(0, 255) );
+    delay(100);
+}
 
-    if (switchState == LOW) {
-        digitalWrite(greenLed, HIGH); 
-        digitalWrite(yellowLed, LOW); 
-        digitalWrite(redLed, LOW); 
-    } else {
-        digitalWrite(greenLed, LOW);
-        digitalWrite(yellowLed, LOW);
-        digitalWrite(redLed, HIGH);
-        delay(250); 
-        digitalWrite(yellowLed. HIGH);
-        digitalWrite(redLed, LOW);
-        delay(250); 
-    }
+void fadeLED( int pin, int brightness ){
+    analogWrite(pin, brightness);
+}
+
+
+int getRandomInt( int min, int max) {
+    return abs( random( min, max ) );
 }
