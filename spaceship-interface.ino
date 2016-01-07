1675025767a73ef6206f396e7d9d8fbb7a3eb511
adriaanbalt <adriaan@balt.us>
2016-01-07 10:39:55 -0500

    spaceship comments added

--- spaceship-interface.ino
+++ spaceship-interface.ino
@@ -2,16 +2,10 @@
  Random Brightness for LEDs
  */
 
-const int greenled = 3;           // the pin that the LED is attached to
-const int yellowled = 5;           // the pin that the LED is attached to
+const int greenled = 3;         // the pin that the LED is attached to
+const int yellowled = 5;        // the pin that the LED is attached to
 const int redled = 6;           // the pin that the LED is attached to
-const int switchPin = 2;           // the pin that the LED is attached to
-
-int redBrightness = 0;    // how bright the LED is
-int greenBrightness = 0;    // how bright the LED is
-int yellowBrightness = 0;    // how bright the LED is
-
-int fadeAmount = 5;    // how many points to fade the LED by
+const int switchPin = 2;        // the pin that the SWITCH is attached to
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -19,22 +13,26 @@ void setup() {
     pinMode(greenled, OUTPUT);
     pinMode(yellowled, OUTPUT);
     pinMode(redled, OUTPUT);
-    pinMode(switchPin, INPUT);
+    pinMode(switchPin, INPUT); // still to hook up
 }
 
-// the loop routine runs over and over again forever:
+// use helper functions to 
 void loop() {
-    fadeLED( greenled, getRandomInt(0, 255) );
-    fadeLED( redled, getRandomInt(0, 255) );
-    fadeLED( yellowled, getRandomInt(0, 255) );
+    // adjust brightness for green led
+    brightnessLED( greenled, getRandomInt(0, 255) );
+    // adjust brightness for red led
+    brightnessLED( redled, getRandomInt(0, 255) );
+    // adjust brightness for yellow led
+    brightnessLED( yellowled, getRandomInt(0, 255) );
     delay(100);
 }
 
-void fadeLED( int pin, int brightness ){
+// update a designated LED to a specific brightness
+void brightnessLED( int pin, int brightness ){
     analogWrite(pin, brightness);
 }
 
-
+// return the absolute value of a number between a max and min
 int getRandomInt( int min, int max) {
     return abs( random( min, max ) );
 }
